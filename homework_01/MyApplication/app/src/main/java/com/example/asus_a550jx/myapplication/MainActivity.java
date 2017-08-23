9e00790de3d8805a0ad681c2beb63dcda0348d2d
noxusfrosted <ntpm96@hotmail.com>
2017-08-23 18:25:09 +0700

    homework 2 done

---
+++ homework_01/MyApplication/app/src/main/java/com/example/asus_a550jx/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asus_a550jx.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homework_02);
+    }
+}
